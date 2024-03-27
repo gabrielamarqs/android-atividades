a05d3a8c77ae80cae40213edcbf06465df2cf2f5
gabrielamarqs <gabriela2013.ms@gmail.com>
2024-03-26 22:04:37 -0300

    atualização

--- Atividade4/app/src/main/java/com/example/atividade4/MainActivity.java
+++ Atividade4/app/src/main/java/com/example/atividade4/MainActivity.java
@@ -1,8 +1,15 @@
 package com.example.atividade4;
 
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.SpannableStringBuilder;
 import android.text.TextWatcher;
+import android.text.style.StyleSpan;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.SeekBar;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,10 +19,13 @@ import androidx.core.view.WindowInsetsCompat;
 
 import com.example.atividade4.databinding.ActivityMainBinding;
 
-public class MainActivity extends AppCompatActivity implements SeekBar.OnSeekBarChangeListener {
+public class MainActivity extends AppCompatActivity implements SeekBar.OnSeekBarChangeListener, View.OnClickListener {
     private ActivityMainBinding binding;
     private TextWatcher inputTextWatcher;
+    private EditText editText;
+    private TextView textView;
 
+    SpannableStringBuilder builder=new SpannableStringBuilder();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,16 +33,15 @@ public class MainActivity extends AppCompatActivity implements SeekBar.OnSeekBar
         setContentView(binding.getRoot());
 
         binding.skbTamanho.setOnSeekBarChangeListener(this);
-        String texto = binding.edtTexto.getText().toString();
-        binding.txtEscrita.setText(texto);
+        binding.btnExibir.setOnClickListener(this);
+        binding.chkItalico.setOnClickListener(this);
+        binding.chkNegrito.setOnClickListener(this);
 
-//        TextWatcher inputTextWatcher1 = new TextWatcher() {
-//            public void afterTextChanged(Editable s) {
-//                number = Integer.parseInt(s.toString());
-//                sum+=number;
-//                tv.setText("sum is "+sum);
-//
-//            }
+
+//        String texto = binding.edtTexto.getText().toString();
+//        TextView txtEscrita = findViewById(R.id.txtEscrita);
+//        txtEscrita.addTextChangedListener(inputTextWatcher);
+
 
     }
 
@@ -50,4 +59,64 @@ public class MainActivity extends AppCompatActivity implements SeekBar.OnSeekBar
     public void onStopTrackingTouch(SeekBar seekBar) {
 
     }
+
+    @Override
+    public void onClick(View view) {
+        String texto = binding.edtTexto.getText().toString();
+        Typeface italicTypeface = Typeface.defaultFromStyle(Typeface.ITALIC); //itálico
+        if (view.getId() == R.id.btnExibir) {
+            binding.txtEscrita.setText(texto);
+        }
+
+        if (view.getId() == R.id.chkMaiuscula ) {
+            String textoCaps = "";
+            if (binding.chkMaiuscula.isChecked()) {
+                textoCaps = texto.toUpperCase();
+            } else {
+                textoCaps = texto.toLowerCase();
+            }
+            binding.txtEscrita.setText(textoCaps);
+        }
+        if (view.getId() == R.id.chkNegrito) {
+            int estilo = Typeface.NORMAL;
+//            String textoNegrito = "";
+
+            if (binding.chkNegrito.isChecked()) {
+                estilo += Typeface.BOLD;
+            } else {
+                estilo += Typeface.NORMAL;
+            }
+//            builder.setSpan(new StyleSpan(Typeface.BOLD), textoNegrito, 0);
+            binding.txtEscrita.setText(texto);
+            binding.txtEscrita.setTypeface(null, estilo);
+        }
+    }
+
+//    public static SpannableStringBuilder makeSectionOfTextBold(String text, String textToBold){
+//
+//        SpannableStringBuilder builder=new SpannableStringBuilder();
+//
+//        if(textToBold.length() > 0 && !textToBold.trim().equals("")){
+//
+//            //for counting start/end indexes
+//            String testText = text.toLowerCase(Locale.US);
+//            String testTextToBold = textToBold.toLowerCase(Locale.US);
+//            int startingIndex = testText.indexOf(testTextToBold);
+//            int endingIndex = startingIndex + testTextToBold.length();
+//            //for counting start/end indexes
+//
+//            if(startingIndex < 0 || endingIndex <0){
+//                return builder.append(text);
+//            }
+//            else if(startingIndex >= 0 && endingIndex >=0){
+//
+//                builder.append(text);
+//                builder.setSpan(new StyleSpan(Typeface.BOLD), startingIndex, endingIndex, 0);
+//            }
+//        }else{
+//            return builder.append(text);
+//        }
+//
+//        return builder;
+//    }
 }
