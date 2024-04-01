2d51949bbaaeff1d61c11f2c062b52fc5f4c35af
gabrielamarqs <gabriela2013.ms@gmail.com>
2024-04-01 17:14:51 -0300

    finalizado

--- Atividade4/app/src/main/java/com/example/atividade4/MainActivity.java
+++ Atividade4/app/src/main/java/com/example/atividade4/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.atividade4;
 
+import android.annotation.SuppressLint;
+import android.graphics.Color;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.text.Editable;
@@ -8,6 +10,7 @@ import android.text.TextWatcher;
 import android.text.style.StyleSpan;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.RadioButton;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
@@ -21,11 +24,7 @@ import com.example.atividade4.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity implements SeekBar.OnSeekBarChangeListener, View.OnClickListener {
     private ActivityMainBinding binding;
-    private TextWatcher inputTextWatcher;
-    private EditText editText;
-    private TextView textView;
-
-    SpannableStringBuilder builder=new SpannableStringBuilder();
+    private Integer tamanhoLetra;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,27 +35,7 @@ public class MainActivity extends AppCompatActivity implements SeekBar.OnSeekBar
         binding.btnExibir.setOnClickListener(this);
         binding.chkItalico.setOnClickListener(this);
         binding.chkNegrito.setOnClickListener(this);
-
-
-//        String texto = binding.edtTexto.getText().toString();
-//        TextView txtEscrita = findViewById(R.id.txtEscrita);
-//        txtEscrita.addTextChangedListener(inputTextWatcher);
-
-
-    }
-
-    @Override
-    public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
-
-    }
-
-    @Override
-    public void onStartTrackingTouch(SeekBar seekBar) {
-
-    }
-
-    @Override
-    public void onStopTrackingTouch(SeekBar seekBar) {
+        binding.rdbCor.setOnClickListener(this);
 
     }
 
@@ -79,17 +58,58 @@ public class MainActivity extends AppCompatActivity implements SeekBar.OnSeekBar
         }
         if (view.getId() == R.id.chkNegrito) {
             int estilo = Typeface.NORMAL;
-//            String textoNegrito = "";
 
             if (binding.chkNegrito.isChecked()) {
                 estilo += Typeface.BOLD;
             } else {
                 estilo += Typeface.NORMAL;
             }
-//            builder.setSpan(new StyleSpan(Typeface.BOLD), textoNegrito, 0);
             binding.txtEscrita.setText(texto);
             binding.txtEscrita.setTypeface(null, estilo);
         }
+        if (view.getId() == R.id.chkItalico) {
+            int estilo = Typeface.NORMAL;
+
+            if (binding.chkItalico.isChecked()) {
+                estilo += Typeface.ITALIC;
+            } else {
+                estilo += Typeface.NORMAL;
+            }
+            binding.txtEscrita.setText(texto);
+            binding.txtEscrita.setTypeface(null, estilo);
+        }
+
+        if (binding.rdbCor.getId() != View.NO_ID) {
+            RadioButton rdbSelecionado = findViewById(binding.rdbCor.getId());
+            String black = "#000000";
+            String vermelho = "#FF0000";
+            String azul = "#0000ff";
+
+            binding.rdbPreto.setText(black);
+            binding.rdbVerm.setText(vermelho);
+            binding.rdbAzul.setText(azul);
+
+            binding.txtEscrita.setTextColor(Color.parseColor(rdbSelecionado.getText().toString()));
+        }
+    }
+
+    @Override
+    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+        if (seekBar.getId() == R.id.skbTamanho) {
+            float tamanhoTexto = progress * 1.0f;
+            binding.txtTamanho.setText((int) tamanhoTexto);
+            binding.txtEscrita.setTextSize(tamanhoTexto);
+        }
+    }
+
+    @Override
+    public void onStartTrackingTouch(SeekBar seekBar) {
+
+    }
+
+    @Override
+    public void onStopTrackingTouch(SeekBar seekBar) {
+
     }
 
 //    public static SpannableStringBuilder makeSectionOfTextBold(String text, String textToBold){
